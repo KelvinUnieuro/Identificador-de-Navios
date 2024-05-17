e4b1ee76827c7d2e612512646498fe64839ab1d2
Guilherme Braga Rios da Costa <guilhermebragaepic2@gmail.com>
2024-05-17 15:39:45 -0300

    [main] A caminho do funcionamento serial

---
+++ OpenCv.py
@@ -0,0 +1,49 @@
+import cv2 as cv
+
+# Para carregar o arquivo xml já treinado
+carregaAlgoritmo = cv.CascadeClassifier('testes-1/haarcascades/first_cascade.xml')
+
+# Pegando a imagem
+# imagem = cv.imread('testes-1/assets/img-boat.jpg')
+# imagem = cv.imread('testes-1/assets/barco4.jpg')
+# imagem = cv.imread('testes-1/assets/barco3.jpg')
+# imagem = cv.imread('testes-1/assets/barco2.jpg')
+# imagem = cv.imread('testes-1/assets/barco.jpg')
+# imagem = cv.imread('testes-1/assets/barco5.jpg')
+# imagem = cv.imread('testes-1/assets/barco6.jpg')
+# imagem = cv.imread('testes-1/assets/barco7.jpg')
+# imagem = cv.imread('testes-1/assets/barco8.jpg')
+# imagem = cv.imread('testes-1/assets/barco9.jpg')
+# imagem = cv.imread('testes-1/assets/barco10.jpg')
+# imagem = cv.imread('testes-1/assets/barco11.jpg')
+imagem = cv.imread('testes-1/assets/barco12.jpg')
+# imagem = cv.imread('testes-1/assets/barco13.jpg')
+# imagem = cv.imread('testes-1/assets/barco14.jpg')
+# imagem = cv.imread('testes-1/assets/barco15.jpg')
+# imagem = cv.imread('testes-1/assets/barco16.jpg')
+# imagem = cv.imread('testes-1/assets/barco17.jpg')
+# imagem = cv.imread('testes-1/assets/barco18.jpg')
+# imagem = cv.imread('testes-1/assets/barco19.jpg')
+
+
+# Deixando a imagem cinza para maior eficiência do opencv
+if imagem is None:
+    print("Erro ao carregar a imagem")
+else:
+    imagemCinza = cv.cvtColor(imagem, cv.COLOR_BGR2GRAY)
+
+boats = carregaAlgoritmo.detectMultiScale(imagemCinza)
+
+print (boats)
+
+for (x, y, l, a) in boats:
+    cv.rectangle(imagem, (x, y), (x + l, y + a), (0, 255, 0), 2)
+
+largura = 1400
+# altura = int(largura * imagem.shape[0] / imagem.shape[1])  # Mantendo a proporção da imagem
+altura = 720
+imagem = cv.resize(imagem, (largura, altura))
+
+cv.imshow('Boats', imagem)
+cv.waitKey(0)
+
---
+++ testes/search-boats-passo-a-passo.md
@@ -0,0 +1,14 @@
+# Search Boats - identificador de navios em imagens em alto mar via satélite, com programação paralela
+
+## Escopo do projeto
+1- O projeto deve ser feito em python, utilizando bibliotecas facilitadoras  
+2- Busca de imagens do mar via satélite (para ser imagens grandes)  
+* Caso não haja imagens via satélite, criar imagens grandes, com cerca de 35gb se possível, com várias imagens do mar "mergeadas" e com navios nessa imagem.  
+
+3- Busca do algoritmo para identificar essas imagens de forma serial;
+4- Paralelização do código para funcionamento eficiente e com Threads;
+5- Testes de processamento na forma serial e com Threads;
+6- Gráficos de SpeedUp e Eficiência do multiprocessamento.
+
+
+
